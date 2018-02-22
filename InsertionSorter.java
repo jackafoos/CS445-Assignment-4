30db3eeace043b732dcce5fa709bb18e6b40aa31
Jackafoos <jem319@pitt.edu>
2018-02-22 15:28:21 -0500

    Class for insertion sort

---
+++ InsertionSorter.java
@@ -0,0 +1,30 @@
+public class InsertionSorter implements IntSorter{
+  /**
+   * This method should initialize your class - reset the # of moves and perform any setup necessary.
+   */
+  public void init(int[] a){
+    this.array = a;
+    moves = 0;
+  }
+
+  /**
+   * This method should sort the values from init.
+   */
+public void sort(){
+
+}
+
+  /**
+   * This method should return the number of moves made in your sort.
+   */
+  public int getMoves(){
+
+  }
+
+  /**
+   * This method should return the number of nanoseconds it took to *only* perform your sort.
+   */
+  public long getSortTime(){
+
+  }
+}
